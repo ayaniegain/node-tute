c18d239385609075622e00c656112bcbcf165a51
ayaniegain <ayaniegain@gmail.com>
2024-02-19 21:56:19 +0530

    update

--- express/index.js
+++ express/index.js
@@ -1,16 +1,39 @@
 import express from "express";
 
+import product from "./product.js";
 
-const app= express()
+const app = express();
 
-let PORT=process.env.PORT || 8000
+let PORT = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-    res.status(201).send('Hello World!')
-  })
+app.get("/", (req, res) => {
+  res.status(201).send("Hello World!");
+});
 
+app.get("/api/users/", (req, res) => {
+  res.status(201).json({ name: "ayan" });
+});
+
+app.get("/api/products/:id", (req, res) => {
+  let productId = Number(req.params.id);
+  if (isNaN(productId)) res.status(404).send("invalid params");
+
+  let singleProduct = product.find((e) => e.id == productId);
+
+  res.status(200).send(singleProduct);
+});
+
+
+app.get("/api/test/", (req, res) => {
+
+    let {page,limit} = req.query;
+
+    console.log(page,limit);
+    res.status(201).json({ name: "ayan" });
+  });
   
 
-app.listen(PORT,()=>{
-    console.log(`port running on ${PORT}`);
-})
+
+app.listen(PORT, () => {
+  console.log(`port running on ${PORT}`);
+});
---
+++ express/product.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        id: 1,
+        productName: "Laptop",
+        price: 999.99
+    },
+    {
+        id: 2,
+        productName: "Smartphone",
+        price: 599.99
+    },
+    {
+        id: 3,
+        productName: "Tablet",
+        price: 399.99
+    },
+    {
+        id: 4,
+        productName: "Headphones",
+        price: 99.99
+    },
+    {
+        id: 5,
+        productName: "Smart Watch",
+        price: 199.99
+    },
+    {
+        id: 6,
+        productName: "TV",
+        price: 1499.99
+    },
+    {
+        id: 7,
+        productName: "Camera",
+        price: 799.99
+    },
+    {
+        id: 8,
+        productName: "Gaming Console",
+        price: 399.99
+    },
+    {
+        id: 9,
+        productName: "Printer",
+        price: 299.99
+    },
+    {
+        id: 10,
+        productName: "Wireless Earbuds",
+        price: 149.99
+    }
+];
