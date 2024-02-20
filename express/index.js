2034739925492d89efa3f69261e8a3292ca25f1c
ayaniegain <ayaniegain@gmail.com>
2024-02-21 00:07:57 +0530

    update joi validator

--- express/index.js
+++ express/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import product from "./product.js";
+import Joi from "joi";
 import { v4 as uuidv4 } from "uuid";
 
 const app = express();
@@ -26,26 +27,41 @@ const commonIndex = (req, res, next) => {
 app.use(loginMiddleware);
 
 //GET----
-
+ 
 app.get("/", (req, res) => {
   console.log("hello", req.name);
 
   res.status(201).send("Hello World!");
 });
 
-app.get("/api/products/", (req, res) => {
-  res.status(201).send(product);
-});
 
-app.get("/api/products/:id", commonIndex, (req, res) => {
+
+app.get("/api/users/:id", commonIndex, (req, res) => {
   let { productIndex, id, body } = req;
   if (isNaN(id)) res.status(404).send("invalid params");
   let singleProduct = product[productIndex];
   res.status(200).send(singleProduct);
 });
 
-app.get("/api/test/", (req, res) => {
+const querySchema = Joi.object({
+  filter: Joi.string().valid('name', 'price').required(),
+  value: Joi.string().required()
+});
+
+// Middleware to validate query parameters
+const validateQuery = (req, res, next) => {
+  const { error, value } = querySchema.validate(req.query);
+  if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+  }
+  req.validatedQuery = value; // Save validated query parameters to req object
+  next();
+};
+
+
+app.get("/api/users/", validateQuery,(req, res) => {
   //QUERY
+
   let { filter, value } = req.query;
 
   let filteredItem = product.filter((item) =>
