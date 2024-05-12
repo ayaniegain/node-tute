7f1818e4f68029d9750e18d18caf30a13d4cbf84
ayaniegain <ayaniegain@gmail.com>
2024-05-12 18:02:07 +0530

    update

--- express course/model/model.schema.js
+++
@@ -1,7 +0,0 @@
-
-import Joi from "joi";
-
-export  const querySchema = Joi.object({
-    filter: Joi.string().valid('name', 'price').required(),
-    value: Joi.string().required()
-  });
--- node js course 2/db.js
+++ node js course 2/db.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
       const conn = await mongoose.connect(`mongodb://localhost:27017/hotel`, {
-        useNewUrlParser: true,
+        // useNewUrlParser: true,
       });
-      console.log(`MongoDB Connected: {conn.connection.host}`);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
       console.error(error.message);
       process.exit(1);
